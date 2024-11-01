78c389ce9b6506e760c65b14c793f3259087c059
Aayush Gupta <aayushgupta219@gmail.com>
2024-11-01 07:12:11 +0300

    moon: BoardConfig: Inherit sepolicy for mediatek devices There are lot of denials which are not device-specific. Henceforth, move such denials to a seperate sepolicy repository and inherit from it.
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Signed-off-by: jacksproject <contact@jackbd.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
