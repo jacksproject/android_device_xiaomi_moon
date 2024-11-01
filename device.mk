c9d36021d45eb90558d8563ff00c6b126713239c
Woomymy <woomy@woomy.be>
2024-11-01 06:27:00 +0300

    moon: Switch to AIDL MultiHAL sensor service * Introduced in Android T
    Test: Boots, `getprop init.svc.vendor.sensors-hal-multihal == running`, `dumpsys sensorservice`, sensors work
    
    Signed-off-by: jacksproject <contact@jackbd.me>

--- device.mk
+++ device.mk
@@ -211,7 +211,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.1.vendor \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.multihal
+    android.hardware.sensors-service.multihal
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
