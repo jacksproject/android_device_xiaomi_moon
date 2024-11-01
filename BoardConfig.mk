6e9a3250fb7cd7f6e00e17589c41c99dc5873d66
sabrina010209 <mcpe11640@gmail.com>
2024-11-01 08:29:29 +0300

    moon: BoardConfig: Inherit common FCM Signed-off-by: jacksproject <contact@jackbd.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,8 @@ TARGET_SCREEN_DENSITY := 320
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
